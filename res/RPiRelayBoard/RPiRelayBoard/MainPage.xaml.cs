04e96a320a47143d7cf547ed74fcb71b79eb2e98
lets-blu <lets_blu@outlook.com>
2018-10-18 16:29:14 +0800

    add cs support

---
+++ res/RPiRelayBoard/RPiRelayBoard/MainPage.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.I2c;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace RPiRelayBoard
+{
+    public sealed partial class MainPage : Page
+    {
+        private RPiRelayBoard board;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            board = new RPiRelayBoard();
+
+            cboRelay.SelectionChanged += UpdateButtonContent;
+            btnRelayControl.Click += UpdateButtonContent;
+            btnRelayToggle.Click += UpdateButtonContent;
+            btnOnAllRelays.Click += UpdateButtonContent;
+            btnOffAllRelays.Click += UpdateButtonContent;
+        }
+
+        private void Grid_Loading(FrameworkElement sender, object args)
+        {
+            for (int i = 0; i < RPiRelayBoard.RelayCount; i++)
+            {
+                cboRelay.Items.Add(i + 1);
+            }
+        }
+
+        private void btnControlRelay_Click(object sender, RoutedEventArgs e)
+        {
+            var relay = 0;
+            if (cboRelay.SelectedItem != null 
+                && !int.TryParse(cboRelay.SelectedItem.ToString(), out relay))
+            {
+                return;
+            }
+
+            switch (btnRelayControl.Content)
+            {
+                case "On":
+                    board.OnRelay(relay);
+                    break;
+
+                case "Off":
+                    board.OffRelay(relay);
+                    break;
+            }
+        }
+
+        private void btnRelayToggle_Click(object sender, RoutedEventArgs e)
+        {
+            var relay = 0;
+            if (cboRelay.SelectedItem != null 
+                &&!int.TryParse(cboRelay.SelectedItem.ToString(), out relay))
+            {
+                return;
+            }
+
+            board.ToggleRelay(relay);
+        }
+
+        private void btnOnAllRelays_Click(object sender, RoutedEventArgs e)
+        {
+            board.OnAllRelays();
+        }
+
+        private void btnOffAllRelays_Click(object sender, RoutedEventArgs e)
+        {
+            board.OffAllRelays();
+        }
+
+        private void UpdateButtonContent(object sender, RoutedEventArgs e)
+        {
+            var relay = 0;
+            if (cboRelay.SelectedItem != null
+                && !int.TryParse(cboRelay.SelectedItem.ToString(), out relay))
+            {
+                return;
+            }
+
+            if (board.IsRelayOpen(relay))
+            {
+                btnRelayControl.Content = "Off";
+            }
+            else
+            {
+                btnRelayControl.Content = "On";
+            }
+        }
+    }
+}
---
+++ res/RPiRelayBoard/RPiRelayBoard/RPiRelayBoard.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.I2c;
+
+namespace RPiRelayBoard
+{
+    class RPiRelayBoard
+    {
+        private I2cDevice device;
+        private byte relayStatus;
+
+        public static int RelayCount { get; } = 4;
+        private static readonly byte REGISTER_ADDRESS = 0x06;
+
+        public RPiRelayBoard(int address = 0x20)
+        {
+            relayStatus = 0xFF;
+            FindDevice(address);
+        }
+
+        public bool OnRelay(int relay)
+        {
+            if (relay < 1 || relay > RelayCount)
+            {
+                return false;
+            }
+
+            relayStatus &= (byte)~(0x01 << (relay - 1));
+            return UpdateRelays();
+        }
+
+        public bool OffRelay(int relay)
+        {
+            if (relay < 1 || relay > RelayCount)
+            {
+                return false;
+            }
+
+            relayStatus |= (byte)(0x01 << (relay - 1));
+            return UpdateRelays();
+        }
+
+        public bool OnAllRelays()
+        {
+            relayStatus &= 0xF0;
+            return UpdateRelays();
+        }
+
+        public bool OffAllRelays()
+        {
+            relayStatus |= 0x0F;
+            return UpdateRelays();
+        }
+
+        public bool ToggleRelay(int relay)
+        {
+            if (relay < 1 || relay > RelayCount)
+            {
+                return false;
+            }
+
+            if (IsRelayOpen(relay))
+            {
+                return OffRelay(relay);
+            }
+            else
+            {
+                return OnRelay(relay);
+            }
+        }
+
+        public bool IsRelayOpen(int relay)
+        {
+            if (relay < 1 || relay > RelayCount)
+            {
+                return false;
+            }
+
+            UpdateRelayStatus();
+            return (relayStatus & (0x01 << (relay - 1))) == 0x00;
+        }
+
+        private async void FindDevice(int address)
+        {
+            await FindDeviceAsync(address);
+        }
+
+        private async Task FindDeviceAsync(int address)
+        {
+            var selector = I2cDevice.GetDeviceSelector();
+            IReadOnlyList<DeviceInformation> devices = await DeviceInformation.FindAllAsync(selector);
+
+            var settings = new I2cConnectionSettings(address);
+            device = await I2cDevice.FromIdAsync(devices.First().Id, settings);
+        }
+
+        private bool UpdateRelays()
+        {
+            if (device != null)
+            {
+                var buffer = new byte[] { REGISTER_ADDRESS, relayStatus };
+                var result = device.WritePartial(buffer).Status == I2cTransferStatus.FullTransfer;
+
+                UpdateRelayStatus();
+
+                return result;
+            }
+
+            return false;
+        }
+
+        private void UpdateRelayStatus()
+        {
+            if (device == null)
+            {
+                return;
+            }
+
+            var writeBuffer = new byte[] { REGISTER_ADDRESS };
+            var readBuffer = new byte[1];
+
+            if (device.WriteReadPartial(writeBuffer, readBuffer).Status == I2cTransferStatus.FullTransfer)
+            {
+                relayStatus = readBuffer.First();
+            }
+        }
+    }
+}
